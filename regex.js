41d1b99a12310af1812a791c867d4275fa2b24b1
Akutsang <akuutsang@gmail.com>
2022-10-04 15:55:24 +0100

    validating all form fields

--- regex.js
+++ regex.js
@@ -1,9 +1,11 @@
 
 const inputs = document.querySelectorAll('input');
 const patterns = {
-    telephone: /^\+\d{3}[-]\d{10}$/,
-    username: /^[a-z\d]{5,12}$/i,
-    password: /^[\w@-]{8,20}$/
+     telephone: /^\+\d{3}[-]\d{10}$/,
+     username: /^[a-z\d]{5,12}$/i,
+     password: /^[\w@-]{8,20}$/,
+     slug: /^[a-z\d-]{8-20}$/,
+     email: /^[([a-z\d\.-]+)@([a-z\d]+)\.(a-z){2,8(\.[a-z]{2,8})?}$/
 
 };
 
